1416d1b59360b711b120dd5662a542d0dc01e42e
RickyAndi <ricky3andika@gmail.com>
2017-07-07 13:51:18 +0700

    Chart component

---
+++ src/components/charts/Area.vue
@@ -0,0 +1,47 @@
+<template>
+  <div :id="options.id"></div>
+</template>
+
+<script>
+  export default {
+    name: 'Area-chart',
+    data() {
+      return {
+        chart: null
+      }
+    },
+    props: {
+      options: {
+        type: Object,
+        required: true
+      },
+      data: {
+        type: Array,
+        required: true
+      }
+    },
+    watch: {
+      data(data) {
+        this.$nextTick(() => {
+          this.chart.setData(this.data);
+        });
+      }
+    },
+    mounted() {
+      this.chart = Morris.Area({
+        element: this.options.id,
+        data: this.data,
+        xkey: this.options.xkey,
+        ykeys: this.options.ykeys,
+        labels: this.options.labels,
+        pointSize: this.options.pointSize,
+        hideHover: this.options.hideHover,
+        resize: this.options.resize
+      });
+    }
+  }
+</script>
+
+<style>
+  
+</style>
---
+++ src/components/charts/Donut.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :id="options.id"></div>
+</template>
+
+<script>
+  export default {
+    name: 'Donut-chart',
+    data() {
+      return {
+        chart: null
+      }
+    },
+    props: {
+      options: {
+        type: Object,
+        required: true
+      },
+      data: {
+        type: Array,
+        required: true
+      }
+    },
+    watch: {
+      data(data) {
+        this.$nextTick(() => {
+          this.chart.setData(this.data);
+        });
+      }
+    },
+    mounted() {
+      this.chart = Morris.Donut({
+        element: this.options.id,
+        data: this.data,
+        resize: this.options.resize
+      });
+    }
+  }
+</script>
+
+<style>
+  
+</style>
