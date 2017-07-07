cd2282d289e5f9fa921088df1fdd63b390671533
RickyAndi <ricky3andika@gmail.com>
2017-07-07 13:51:06 +0700

    Dropdown component

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,36 @@
+<template>
+  <li class="dropdown" :class="{'open': open}">
+    <a @mousedown="toggleDropdown" class="dropdown-toggle"><i :class="icon"></i> <b class="caret"></b></a>
+    <slot></slot>
+  </li>
+</template>
+  
+<script>
+  export default {
+    name: 'Dropdown',
+    props: {
+      icon: {
+        type: String,
+        required: true
+      }
+    },
+    data() {
+      return {
+        open: false
+      };
+    },
+    methods: {
+      toggleDropdown() {
+        const openDropdown = !this.open;
+        this.open = openDropdown;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  a { 
+    cursor: pointer; 
+    cursor: hand; 
+  }
+</style>
