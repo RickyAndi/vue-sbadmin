e994ee5e1b78925edd72fc96e12d3571004cd45f
RickyAndi <ricky3andika@gmail.com>
2017-07-07 13:50:45 +0700

    Fix menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,44 +10,65 @@
         <a class="navbar-brand" href="#">SB Admin</a>
     </div>
     <ul class="nav navbar-right top-nav">
-      <ul class="dropdown-menu message-dropdown">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-envelope"></i> <b class="caret"></b></a>
-          <ul class="dropdown-menu message-dropdown">
-            <li class="message-preview">
-              <a href="#">
-                <div class="media">
-                  <span class="pull-left">
-                    <img class="media-object" src="http://placehold.it/50x50" alt="">
-                  </span>
-                  <div class="media-body">
-                    <h5 class="media-heading"><strong>John Smith</strong></h5>
-                    <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                  </div>
+      <li is="Dropdown" :icon="'fa fa-envelope'">
+        <ul class="dropdown-menu message-dropdown">
+          <li v-for="message in messages" class="message-preview">
+            <a href="#">
+              <div class="media">
+                <span class="pull-left">
+                  <img class="media-object" src="http://placehold.it/50x50" alt="">
+                </span>
+                <div class="media-body">
+                  <h5 class="media-heading"><strong>{{ message.from }}</strong>
+                  </h5>
+                  <p class="small text-muted"><i class="fa fa-clock-o"></i>{{ message.time }}</p>
+                  <p>{{ message.content }}</p>
                 </div>
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+              </div>
+            </a>
+          </li>
+          <li class="message-footer">
+            <a href="#">Read All New Messages</a>
+          </li>
+        </ul>
+      </li>
+      <li is="Dropdown" :icon="'fa fa-bell'">
+        <ul class="dropdown-menu alert-dropdown">
+          <li v-for="alert in alerts">
+            <a href="#">{{ alert.message }} <span :class="getAlertLabelClass(alert.type)">{{ alert.name }}</span></a>
+          </li>
+          <li class="divider"></li>
+          <li>
+              <a href="#">View All</a>
+          </li>
+        </ul>
+      </li>
+      <li is="Dropdown" :icon="'fa fa-user'">
+        <ul class="dropdown-menu">
+          <li>
+            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
+          </li>
+          <li>
+            <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
+          </li>
+          <li class="divider"></li>
+          <li>
+            <a href="#"><i class="fa fa-fw fa-power-off"></i> Log Out</a>
+          </li>
+        </ul>
+      </li>
     </ul>
     <div class="collapse navbar-collapse navbar-ex1-collapse">
       <ul class="nav navbar-nav side-nav">
-        <li>
-          <router-link to="/dashboard"><i class="fa fa-fw fa-dashboard"></i> Dashboard</router-link>
-        </li>
-        <li>
-          <router-link to="/table"><i class="fa fa-fw fa-table"></i> Table</router-link>
-        </li>
-        <li>
-          <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-          <ul id="demo" class="collapse">
-              <li>
-                  <a href="#">Dropdown Item</a>
-              </li>
-              <li>
-                  <a href="#">Dropdown Item</a>
+        <li v-for="route in routes">
+          <router-link v-if="!route.hasChild" router-link :to="route.path"><i :class="route.icon"></i> {{ route.name }}</router-link>
+          <a v-if="route.hasChild" href="javascript:;" data-toggle="collapse" :data-target="'#' + route.name"><i class="fa fa-fw fa-arrows-v"></i>{{ route.name }}<i :class="route.icon"></i></a>
+          <ul :id="route.name" class="collapse">
+              <li v-for="child in route.childs">
+                  <router-link :to="child.path"><i :class="child.icon"></i> {{ child.name }}</router-link>
               </li>
           </ul>
         </li>
@@ -57,9 +78,41 @@
 </template>
 
 <script>
-  /* eslint-disable */
+  import Dropdown from './Dropdown'; 
+  import RoutesList from '../routes';
+
   export default {
     name: 'navbar',
+    components: {
+      Dropdown
+    },
+    mounted() {
+      console.log(window.$)
+    },
+    methods: {
+      getAlertLabelClass(alertType) {
+        return 'label label-' + alertType;
+      }
+    },
+    data() {
+      return {
+        alerts: [
+          {
+            type: 'danger',
+            message: 'Makan',
+            name: 'Bahaya'
+          }
+        ],
+        messages: [
+          {
+            from: 'Budi',
+            time: 'Yesterday at 4:32 PM',
+            content: 'Lorem ipsum dolor sit amet'
+          }
+        ],
+        routes: RoutesList
+      };
+    }
   }
 </script>
 
